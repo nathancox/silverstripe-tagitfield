c14459e060183d93a4fae427925cb35563ed6adf
nathancox <me@nathan.net.nz>
2012-04-15 19:19:16 +1200

    general improvements

--- code/TagItField.php
+++ code/TagItField.php
@@ -24,9 +24,28 @@ class TagItField extends TextField {
 	
 	var $availableTags = array();
 	
+	var $tagClass = null;
+	
+	var $tagLabelField = 'Title';
+	
+	var $deleteUnusedTags = true;
+	
+	
 	//var $valueArray = array();
 	
 	function __construct($name, $title = null, $value = "", $form = null) {
+		
+
+		/*
+		if($has_manys = $controller->stat('has_many')) {
+			foreach($has_manys as $relation => $value) {
+				$name = $relation;
+				$sourceClass = $value;
+				break;
+			}
+		}
+		*/
+		
 		parent::__construct($name, $title, $value, null, $form);
 	}
 	
@@ -51,15 +70,17 @@ class TagItField extends TextField {
 	}
 	
 	function Field() {
-		// we have to set this URL at the last minute because $this->Link() does work in a setter
-		if ($this->getIsAJAX()) {
-			$this->settings['tagSourceURL'] = $this->Link('suggest');
-		}
+		
 		$this->setAttribute('data-settings', Convert::array2json($this->settings));
 		$availableTags = $this->getAvailableTags();
 		if (count($availableTags) > 0) {
 			$this->setAttribute('data-available-tags', implode("::", $availableTags));
+		} 
+		if ($this->getIsAJAX()) {
+			// we have to set this URL at the last minute because $this->Link() does work in a setter
+			$this->settings['tagSourceURL'] = $this->Link('suggest');
 		}
+		
 		
 		$html = parent::Field();
 		$this->includeRequirements();
@@ -102,6 +123,13 @@ class TagItField extends TextField {
 		return $this->settings['tagSourceURL'];
 	}
 	
+	function deleteUnusedTags($boolean) {
+		$this->deleteUnusedTags = $boolean;
+	}
+	function getDeleteUnusedTags() {
+		return $this->deleteUnusedTags;
+	}
+	
 	function useAJAX($boolean) {
 		$this->useAJAX = $boolean;
 		/*
@@ -127,18 +155,72 @@ class TagItField extends TextField {
 		return $this->settings[$key];
 	}
 	
+	function setRelation($class = null, $field = null) {
+		if ($class != null) {
+			$this->tagClass = $class;
+		}
+		if ($field != null) {
+			$this->tagLabelField = $field;
+		}
+	}
+	
 	
 	function setAvailableTags($tagArray) {
 		$this->availableTags = $tagArray;
 	}
-
+	
+	/*
+		Get a list of tags for pre-populating the autosuggest
+	*/
 	function getAvailableTags() {
-		return $this->availableTags;
+		$tagArray = array();
+		if (count($this->availableTags) > 0) {
+			$tagArray = $this->availableTags;
+		} else if ($tagClass = $this->getTagClass()) {
+			$labelField = $this->getTagLabelField();
+			if ($labelField) {
+	//		$tags = DataObject::get($tagClass);
+				$tags = DataList::create($tagClass)->sort($labelField);
+				if ($tags) {
+					$tagArray = $tags->map($labelField, $labelField)->toArray();
+				}
+				
+			}
+			
+		}
+		
+		return $tagArray;
 	}
 	
 	
+	
+	/*
+		find out the class name of the tag objects
+	*/
+	function getTagClass() {
+		if ($this->tagClass != null) {
+			return $this->tagClass;
+		} else if ($record = $this->form->record) {
+			if ($relation = $this->getRelation()) {
+				return $relation[1];
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	function getTagLabelField() {
+		return $this->tagLabelField;
+	}
+	
+	/*
+		Action for AJAX autocomplete
+		@TODO: THIS
+	*/
 	function suggest($request) {
 		$searchString = $request->requestVar('search');
+		
 		
 		$output = array();
 		$output[] = 'test';
@@ -150,27 +232,88 @@ class TagItField extends TextField {
 		return $response;
 	}
 	
-	function getTagsFromSource() {
-		$output = array();
-		$output[] = 'test';
-		$output[] = 'testing';
-		
-		return $output;
+	function setValue($value, $obj = null) {
+		if(!$value && $obj && $obj instanceof DataObject && $obj->many_many($this->name)) {
+			$funcName = $this->name;
+			$tags = $obj->$funcName()->map('ID', $this->getTagLabelField())->toArray();
+			$value = implode($this->getDelimiter(), $tags);
+		}
+		$this->value = $value;
+		return $this;
 	}
 	
-	
-	/*
-	function setValueX($value) {
-		$this->valueArray = explode();
+	function saveInto($record) {
+		if ($relation = $this->getRelation()) {
+			$submittedTags = explode($this->getDelimiter(), $this->value);
+			
+			$tagClass = $this->getTagClass();
+			$tagLabelField = $this->getTagLabelField();
+			
+			$tagObjects = DataList::create($tagClass)->filter(
+				$tagLabelField, $submittedTags
+			);
+			
+			if ($tagObjects->Count() < count($submittedTags)) {
+				// filter out the tags that exist already
+				$tagsAsKeys = array_flip($submittedTags);
+				foreach ($tagObjects as $tag) {
+					$label = $tag->{$tagLabelField};
+					unset($tagsAsKeys[$label]);
+				}
+				
+				foreach ($tagsAsKeys as $label => $value) {
+					$tagObject = new $tagClass();
+					$tagObject->$tagLabelField = $label;
+					$tagObject->write();
+					$tagObjects->add($tagObject);
+				}
+			}
+			
+			$relationList = $this->form->record->{$this->name}();
+			$oldTags = $relationList->map('ID', $tagLabelField)->toArray();
+			$relationList->removeAll();
+			$relationList->addMany($tagObjects->toArray());
+			
+			if ($this->deleteUnusedTags) {
+				$deletedTags = array_diff($oldTags, $tagObjects->map('ID', $tagLabelField)->toArray());
+				
+				if (count($deletedTags) > 0) {
+					$relationTable = $relation[4];
+					foreach ($deletedTags as $id => $title) {
+						$query = new SQLQuery();
+						$query->select = array('ID', $tagLabelField);
+						$query->from = array($relationTable);
+						$query->where = array("ID = ".$id);
+						$count = $query->Count();
+						if ($count == 0) {
+							DataObject::delete_by_id($tagClass, $id);
+						}
+					}
+					
+				}
+			}
+			
+		} else if ($record->hasField()) {
+			$record->setCastedField($this->name, $this->dataValue());
+		} else {
+			// @TODO: better error handling
+		}
 		
-		return parent::setValue($value);
+		
 	}
 	
-	function dataValueX() {
-		
+	function getRelation() {
+		$relation = false;
+		if ($manyMany = singleton($this->form->record->class)->many_many($this->name)) {
+			$relation = $manyMany;
+		}
+		/* else if ($hasMany = singleton($this->form->record->class)->has_many($this->name)) {
+			$relation = $hasMany;
+		}
+		*/
+		return $relation;
 	}
-	*/
-	
+
 	
 	
 }
